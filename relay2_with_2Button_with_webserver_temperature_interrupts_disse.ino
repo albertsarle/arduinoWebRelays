567622867f7483fbad88d6429209546f61953383
Albert Sarle <albertsarle@gmail.com>
2014-01-03 20:13:36 +0100

    Codi amb Interrupcions i referencia css i js a dropbox

--- relay2_with_2Button_with_webserver_temperature_interrupts_disse.ino
+++ relay2_with_2Button_with_webserver_temperature_interrupts_disse.ino
@@ -20,12 +20,17 @@ int LM35_2_Pin = 1;
 
 
 /////////  INICI PART WEBSERVER
+
+// la xarxa local de Casserres es aquesta: 
+// nanostation2 (gateway) 192.168.1.1 amb nat al port 19710 cap al 80 del arduino
+// linksys WRT54GL (access point)  192.168.1.2
+// arduino (webserver) 192.168.1.3
 byte mac[] = { 
   0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 byte ip[] = { 
-  192,168, 0, 25 };
+  192,168, 1, 3 };
 byte gateway[] ={ 
-  192, 168, 0, 21 };
+  192, 168, 1, 1 };
 EthernetServer server(80);
 String HttpHeader = String(MaxHeaderLength); 
 
@@ -42,7 +47,12 @@ volatile int val2 = 0;    // el valor que recollim del pin d'entrada
 volatile int buttonState1 = 0;  // variable to hold the last button state
 volatile int buttonState2 = 0;  // variable to hold the last button state
 
-int lightStatus1;
+/// arduino debounce interrupts http://www.instructables.com/id/Arduino-Software-debouncing-in-interrupt-function/
+long debouncing_time = 400; //Debouncing Time in Milliseconds
+volatile unsigned long last_micros1;
+volatile unsigned long last_micros2;
+
+int lightStatus1 = 0;
 int lightStatus2 = 0;  
 
 void initWebServer()
@@ -78,57 +88,49 @@ void webServerDaemon()
         // so you can send a reply
         if (c == '\n' && currentLineIsBlank) {
 
-          Serial.println(HttpHeader);
-
           int api = HttpHeader.indexOf("/api/");
-          
+
           String apiCallback;           
           if (api>0) {
-            
-           
-             int switch1 = HttpHeader.indexOf("switch1");
-             if (switch1>0) {
 
-                int p1 = HttpHeader.indexOf("s=1");
-                Serial.println("P1 = "+ p1);            
-                if (p1>=0) {
-                  digitalWrite(relayPin1, LOW);   
-                  lightStatus1 = 1;
-                  Serial.println("Encenem el llum 1 ");            
-                  apiCallback = "callBack(1, 1);";                  
-                } 
-                else 
-                {
-                  digitalWrite(relayPin1, HIGH);  
-                  lightStatus1 = 0;
-                  Serial.println("Apaguem el llum 1 ");            
-                  apiCallback = "callBack(1, 0);";
-    
-                }
-                   
-             }
-             int switch2 = HttpHeader.indexOf("switch2");
-             if (switch2>0) {
 
-                int p2 = HttpHeader.indexOf("s=1"); 
-                Serial.println("P2 = ");            
-                Serial.println(p2);                        
-                if (p2>0) {
-                  digitalWrite(relayPin2, LOW);   
-                  lightStatus2 = 1;
-                  Serial.println("Encenem el llum 2 ");            
-                  apiCallback = "callBack(2, 1);";
-                } 
-                else 
-                {
-                  digitalWrite(relayPin2, HIGH);  
-                  lightStatus2 = 0;
-                  Serial.println("Apaguem el llum 2 ");            
-                  apiCallback = "callBack(2, 0);";                  
-                } 
-             }            
+            int switch1 = HttpHeader.indexOf("switch1");
+            if (switch1>0) {
+
+              int p1 = HttpHeader.indexOf("s=1");
+
+              if (p1>=0) {
+                digitalWrite(relayPin1, LOW);   
+                lightStatus1 = 1;
+                apiCallback = "callBack(1, 1);";                  
+              } 
+              else 
+              {
+                digitalWrite(relayPin1, HIGH);  
+                lightStatus1 = 0;
+                apiCallback = "callBack(1, 0);";
+
+              }
+
+            }
+            int switch2 = HttpHeader.indexOf("switch2");
+            if (switch2>0) {
+
+              int p2 = HttpHeader.indexOf("s=1"); 
+              if (p2>0) {
+                digitalWrite(relayPin2, LOW);   
+                lightStatus2 = 1;
+                apiCallback = "callBack(2, 1);";
+              } 
+              else 
+              {
+                digitalWrite(relayPin2, HIGH);  
+                lightStatus2 = 0;
+                apiCallback = "callBack(2, 0);";                  
+              } 
+            }            
           }          
-
+          
           String classLightStatus1;
           String classLightStatus2;
 
@@ -159,41 +161,40 @@ void webServerDaemon()
             client.println("<!doctype html><html lang=\"en\"><head><meta charset=\"utf-8\"><title>Connectant un arduino a guifi.net 1</title>");
             client.println("<meta name=\"author\" content=\"albertsarle@gmail.com\">");
             client.println("<script src=\"http://code.jquery.com/jquery-1.10.1.min.js\"></script>");
-            client.println("<script src=\"http://ximples.com/temp.js\"></script>");
-            client.println("<link href=\"http://ximples.com/temp.css\" rel=\"stylesheet\" type=\"text/css\" />");
+            client.println("<script src=\"http://dl.dropboxusercontent.com/u/1834023/arduino/temp.js\"></script>");
+            client.println("<link href=\"http://dl.dropboxusercontent.com/u/1834023/arduino/temp.css\" rel=\"stylesheet\" type=\"text/css\" />");
             client.println("</head><body><div id=\"container\">");
             client.println("<div class=\"block temperature\">out ");
-            
+
             char buffer[10];
             String temperaturaS = dtostrf(temperatura, 5, 1, buffer);            
             client.println(temperaturaS);
-            
+
             client.println(" °C</div>");
             client.println("<div class=\"block temperature\">in ");
 
             String LM35_1S = dtostrf(LM35_1, 5, 1, buffer);            
             client.println(LM35_1S);
-            
+
             client.println(" °C</div>");
             client.println("<div class=\"block\" id=\"humidity\">");
-            
+
             String humitatS = dtostrf(humitat, 5, 1, buffer);            
             client.println(humitatS);
-            
+
             client.println(" %</div>");
             client.println("<div class=\"block switch ");
             client.println(classLightStatus1);
             client.println("\" id=\"switch1\">heat is <span>");
             client.println(classLightStatus1);
             client.println("</span></div>");
-  
+
             client.println("<div class=\"block switch ");
             client.println(classLightStatus2);
-            client.println("\" id=\"switch2\">water is d
-            <span>");
+            client.println("\" id=\"switch2\">water is <span>");
             client.println(classLightStatus2);
             client.println("</span></div>");
-  
+
             client.println("</div>");
             client.println("</body></html>");          
           }
@@ -237,8 +238,57 @@ void buttonRelaysInit() {
   buttonState2 = digitalRead(switchPin2);   // read the initial state  
 }
 
+void processRelays()
+{
+  val1 = digitalRead(switchPin1);   // read input value and store it in val 
+  val2 = digitalRead(switchPin2);   // read input value and store it in val   
+
+  if (val1 != buttonState1) {          // the button state has changed!
+    if (val1 == HIGH) {                // check if the button is pressed
+      Serial.println("Button 1 just pressed");
+      if (lightStatus1 == 1){
+          digitalWrite(relayPin1, HIGH);   // energizes the relay and lights the LEDx
+          lightStatus1 = 0;
+           Serial.println("Apaguem el llum 1 ");
+         
+      } else {
+          digitalWrite(relayPin1, LOW);   // energizes the relay and lights the LEDx
+          lightStatus1 = 1;
+           Serial.println("Engeguem el llum 1 ");
+          
+      }       
+    } else {                         // the button is -not- pressed...
+      Serial.println("Button 1 just released");
+    }
+  }
+ 
+  if (val2 != buttonState2) {          // the button state has changed!
+    if (val2 == HIGH) {                // check if the button is pressed
+      Serial.println("Button 2 just pressed");
+      if (lightStatus2 == 1){
+          digitalWrite(relayPin2, HIGH);   // energizes the relay and lights the LEDx
+          lightStatus2 = 0;
+           Serial.println("Engeguem el llum 2 ");
+      } else {
+          digitalWrite(relayPin2, LOW);   // energizes the relay and lights the LEDx
+          lightStatus2 = 1;
+           Serial.println("Apaguem el llum 2 ");         
+      }       
+    } else {                         // the button is -not- pressed...
+      Serial.println("Button 2 just released");
+    }
+  }
+ 
+ 
+  //Serial.println(val1);
+  buttonState1 = val1;                 // save the new state in our variable
+  buttonState2 = val2;                 // save the new state in our variable 
+}
+
+
 void processRelay1()
 {
+
   if (lightStatus1 == 0){
     digitalWrite(relayPin1, LOW);  
     lightStatus1 = 1;
@@ -280,14 +330,28 @@ void temperature()
 
 }
 
+void debounceInterrupt1() {
+  if((long)(micros() - last_micros1) >= debouncing_time * 1000) {
+    processRelay1();
+    last_micros1 = micros();
+  }
+}
+
+void debounceInterrupt2() {
+  if((long)(micros() - last_micros2) >= debouncing_time * 1000) {
+    processRelay2();
+    last_micros2 = micros();
+  }
+}
+
 void setup() {                
   Serial.begin(9600);
   temperature();
   buttonRelaysInit();
   initWebServer();
 
-  attachInterrupt(0, processRelay1, FALLING);
-  attachInterrupt(1, processRelay2, FALLING);
+  attachInterrupt(0, debounceInterrupt1, FALLING);
+  attachInterrupt(1, debounceInterrupt2, FALLING);
   lightStatus1 =0;
   lightStatus2 =0;  
 }
@@ -298,9 +362,3 @@ void loop() {
   webServerDaemon(); 
   delay(100);       
 }
-
-
-
-
-
-
